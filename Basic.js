3bccd555ebe04a30e34519b5f9f4898197b7ea67
VaderSteve76 <alverson.s@yahoo.com>
2020-02-11 18:56:03 -0500

    Initial Commit

---
+++ Basic.js
@@ -0,0 +1,47 @@
+// this is a line-comment
+
+/* this is a 
+  multi line comment */
+
+  // Example
+var ourName;
+
+// Declare myName below this line
+var myName;
+
+// Setup
+var a;
+var b = 2;
+
+// Only change code below this line
+// Assign the value 7 to variable a.
+// Assign the contents of a to variable b.
+var a = 7;
+var b = a;
+
+
+/* Initialize these three variables
+Initialize the three variables a, b, and c with 5, 10, and "I am a" 
+respectively so that they will not be undefined. */
+var a = 5;
+var b = 10;
+var c = "I am a";
+// Do not change code below this line
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+
+/* Write variable names in JavaScript in camelCase. 
+In camelCase, multi-word variable names have the first word 
+in lowercase and the first letter of each subsequent word is capitalized.
+*/ 
+// Declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
