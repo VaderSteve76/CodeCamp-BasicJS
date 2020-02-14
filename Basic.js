d1a1af3944ddcf3730b3067cd879ccc8835bf563
VaderSteve76 <alverson.s@yahoo.com>
2020-02-14 18:12:40 -0500

    Reusable functions, GLobla & local scope, retirning values

--- Basic.js
+++ Basic.js
@@ -658,3 +658,260 @@ myArray.shift();
 myArray.unshift(["Paul", 35]);
 
 
+// Create a shopping list in the variable myList. 
+// The list should be a multi-dimensional array containing several sub-arrays.
+// The first element in each sub-array should contain a string with the 
+// name of the item. The second element should be a number representing the quantity i.e.
+// ["Chocolate Bar", 15]
+// There should be at least 5 sub-arrays in the list.
+var myList = [
+  ["Steve", 43],
+  ["Donna", 64],
+  ["Skyler", 22],
+  ["Cai", 28],
+  ["Jen", 30]
+];
+
+
+// In JavaScript, we can divide up our code 
+// into reusable parts called functions.
+// Here's an example of a function:
+
+// function functionName() {
+//   console.log("Hello World");
+// }
+
+// You can call or invoke this function by using 
+// its name followed by parentheses, like this: 
+// functionName(); Each time the function is called 
+// it will print out the message "Hello World" on 
+// the dev console. All of the code between the curly 
+// braces will be executed every time the function is called.
+
+// Create a function called reusableFunction which prints "Hi World" to the dev console.
+// Call the function.
+
+// Example
+function ourReusableFunction() {
+  console.log("Heyya, World");
+}
+
+ourReusableFunction();
+// Only change code below this line
+function reusableFunction() {
+  console.log("Hi World");
+}
+
+reusableFunction();
+
+
+// Parameters are variables that act as placeholders 
+// for the values that are to be input to a function 
+// when it is called. When a function is defined, it is 
+// typically defined along with one or more parameters. 
+// The actual values that are input (or "passed") into 
+// a function when it is called are known as arguments.
+// Here is a function with two parameters, param1 and param2:
+
+// function testFun(param1, param2) {
+//   console.log(param1, param2);
+// }
+
+// Then we can call testFun: testFun("Hello", "World"); 
+// We have passed two arguments, "Hello" and "World". 
+// Inside the function, param1 will equal "Hello" and 
+// param2 will equal "World". Note that you could call 
+// testFun again with different arguments and the parameters 
+// would take on the value of the new arguments.
+// Create a function called functionWithArgs that accepts 
+// two arguments and outputs their sum to the dev console.
+// Call the function with two numbers as arguments.
+// Example
+function ourFunctionWithArgs(a, b) {
+  console.log(a - b);
+}
+ourFunctionWithArgs(10, 5); // Outputs 5
+// Only change code below this line.
+function functionWithArgs(a, b) {
+  console.log(a +b);
+}
+functionWithArgs(5, 5);
+
+
+// In JavaScript, scope refers to the visibility of variables. 
+// Variables which are defined outside of a function 
+// block have Global scope. This means, they can be 
+// seen everywhere in your JavaScript code.
+// Variables which are used without the var keyword 
+// are automatically created in the global scope. 
+// This can create unintended consequences elsewhere 
+// in your code or when running a function again. 
+// You should always declare your variables with var.
+// Using var, declare a global variable myGlobal outside 
+// of any function. Initialize it with a value of 10.
+// Inside function fun1, assign 5 to oopsGlobal without using the var keyword.
+
+// Declare your variable here
+var myGlobal = 10;
+
+function fun1() {
+  // Assign 5 to oopsGlobal Here
+oopsGlobal = 5;
+}
+// Only change code above this line
+function fun2() {
+  var output = "";
+  if (typeof myGlobal != "undefined") {
+    output += "myGlobal: " + myGlobal;
+  }
+  if (typeof oopsGlobal != "undefined") {
+    output += " oopsGlobal: " + oopsGlobal;
+  }
+  console.log(output);
+}
+
+
+// Variables which are declared within a function, 
+// as well as the function parameters have local scope. 
+// That means, they are only visible within that function.
+// Here is a function myTest with a local variable called loc.
+
+// function myTest() {
+//   var loc = "foo";
+//   console.log(loc);
+// }
+// myTest(); // logs "foo"
+// console.log(loc); // loc is not defined
+
+// loc is not defined outside of the function.
+// Declare a local variable myVar inside myLocalScope. 
+// Run the tests and then follow the instructions commented out in the editor.
+function myLocalScope() {
+  'use strict'; // you shouldn't need to edit this line
+  var myVar = "Hi";
+  console.log(myVar);
+}
+myLocalScope();
+// Run and check the console
+// myVar is not defined outside of myLocalScope
+console.log(myVar);
+// Now remove the console log line to pass the test
+
+
+// It is possible to have both local and global variables 
+// with the same name. When you do this, the local variable 
+// takes precedence over the global variable.
+
+// In this example:
+
+// var someVar = "Hat";
+// function myFun() {
+//   var someVar = "Head";
+//   return someVar;
+// }
+
+// The function myFun will return "Head" because the 
+// local version of the variable is present.
+// Add a local variable to myOutfit function to 
+// override the value of outerWear with "sweater".
+// Setup
+var outerWear = "T-Shirt";
+
+function myOutfit() {
+  // Only change code below this line
+  var outerWear = "sweater";
+  // Only change code above this line
+  return outerWear;
+}
+
+myOutfit();
+
+
+// We can pass values into a function with arguments. 
+// You can use a return statement to send a value back out of a function.
+
+// Example
+
+// function plusThree(num) {
+//   return num + 3;
+// }
+// var answer = plusThree(5); // 8
+
+// plusThree takes an argument for num and returns a value equal to num + 3.
+
+// Create a function timesFive that accepts one argument, multiplies it by 5, 
+// and returns the new value. See the last line in the editor for an example 
+// of how you can test your timesFive function.
+// Example
+function minusSeven(num) {
+  return num - 7;
+}
+
+// Only change code below this line
+function timesFive(num) {
+  return num * 5;
+}
+console.log(minusSeven(10));
+
+
+// A function can include the return statement but 
+// it does not have to. In the case that the function 
+// doesn't have a return statement, when you call it, 
+// the function processes the inner code but the returned value is undefined.
+
+// Example
+// var sum = 0;
+// function addSum(num) {
+//   sum = sum + num;
+// }
+// addSum(3); // sum will be modified but returned value is undefined
+
+// addSum is a function without a return statement. The 
+// function will change the global sum variable but the 
+// returned value of the function is undefined.
+// Create a function addFive without any arguments. This 
+// function adds 5 to the sum variable, 
+// but its returned value is undefined.
+// Example
+var sum = 0;
+function addThree() {
+  sum = sum + 3;
+}
+// Only change code below this line
+function addFive() {
+  sum = sum + 5;
+}
+// Only change code above this line
+addThree();
+addFive();
+
+
+// If you'll recall from our discussion of Storing 
+// Values with the Assignment Operator, everything to 
+// the right of the equal sign is resolved before the 
+// value is assigned. This means we can take the return 
+// value of a function and assign it to a variable.
+// Assume we have pre-defined a function sum which 
+// adds two numbers together, then:
+// ourSum = sum(5, 12);
+// will call sum function, which returns a value of 
+// 17 and assigns it to ourSum variable.
+// Call the processArg function with an argument of 
+// 7 and assign its return value to the variable processed.
+// Example
+var changed = 0;
+
+function change(num) {
+  return (num + 5) / 3;
+}
+changed = change(10);
+// Setup
+var processed = 0;
+
+function processArg(num) {
+  return (num + 3) / 5;
+}
+// Only change code below this line
+processed = processArg(7);
+
+
