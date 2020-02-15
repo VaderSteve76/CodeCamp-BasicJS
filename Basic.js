511d44a9b5a93ede33ae7d60c41095fa9ff0ee37
VaderSteve76 <alverson.s@yahoo.com>
2020-02-15 16:29:21 -0500

    Compsarison Operators

--- Basic.js
+++ Basic.js
@@ -1177,3 +1177,87 @@ function testGreaterThan(val) {
 testGreaterThan(10);
 
 
+// The greater than or equal to operator (>=) 
+// compares the values of two numbers. If the number 
+// to the left is greater than or equal to the number 
+// to the right, it returns true. Otherwise, it returns false.
+// Like the equality operator, greater than or equal 
+// to operator will convert data types while comparing.
+
+// Examples
+// 6   >=  6   // true
+// 7   >= '3'  // true
+// 2   >=  3   // false
+// '7' >=  9   // false
+
+// Add the greater than or equal to operator to the 
+// indicated lines so that the return statements make sense.
+function testGreaterOrEqual(val) {
+  if (val >= 20) {  // Change this line
+    return "20 or Over";
+  }
+  if (val >= 10) {  // Change this line
+    return "10 or Over";
+  }
+  return "Less than 10";
+}
+// Change this value to test
+testGreaterOrEqual(10);
+
+
+// The less than operator (<) compares the values of two numbers. 
+// If the number to the left is less than the number to the right, 
+// it returns true. Otherwise, it returns false. Like the equality 
+// operator, less than operator converts data types while comparing.
+
+// Examples
+// 2   < 5  // true
+// '3' < 7  // true
+// 5   < 5  // false
+// 3   < 2  // false
+// '8' < 4  // false
+
+// Add the less than operator to the indicated 
+// lines so that the return statements make sense.
+function testLessThan(val) {
+  if (val < 25) {  // Change this line
+    return "Under 25";
+  }
+  if (val < 55) {  // Change this line
+    return "Under 55";
+  }
+  return "55 or Over";
+}
+// Change this value to test
+testLessThan(10);
+
+
+// The less than or equal to operator (<=) compares the 
+// values of two numbers. If the number to the left is less 
+// than or equal to the number to the right, it returns true. 
+// If the number on the left is greater than the number on the 
+// right, it returns false. Like the equality operator, 
+// less than or equal to converts data types.
+
+// Examples
+// 4   <= 5  // true
+// '7' <= 7  // true
+// 5   <= 5  // true
+// 3   <= 2  // false
+// '8' <= 4  // false
+
+// Add the less than or equal to operator to the 
+// indicated lines so that the return statements make sense.
+function testLessOrEqual(val) {
+  if (val <= 12) {  // Change this line
+    return "Smaller Than or Equal to 12";
+  }
+  if (val <= 24) {  // Change this line
+    return "Smaller Than or Equal to 24";
+  }
+  return "More Than 24";
+}
+// Change this value to test
+testLessOrEqual(10);
+
+
