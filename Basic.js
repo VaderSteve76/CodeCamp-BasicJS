46b986497fb1db68a2a6162fca2e56e9b2d9bcdb
VaderSteve76 <alverson.s@yahoo.com>
2020-02-16 15:32:44 -0500

    And & Or Operators

--- Basic.js
+++ Basic.js
@@ -1261,3 +1261,73 @@ function testLessOrEqual(val) {
 testLessOrEqual(10);
 
 
+// Sometimes you will need to test more than one thing at a time. 
+// The logical and operator (&&) returns true if and only if the 
+// operands to the left and right of it are true.
+// The same effect could be achieved by nesting an if statement inside another if:
+
+// if (num > 5) {
+//   if (num < 10) {
+//     return "Yes";
+//   }
+// }
+// return "No";
+
+// will only return "Yes" if num is greater than 5 and less than 10. The same logic can be written as:
+
+// if (num > 5 && num < 10) {
+//   return "Yes";
+// }
+// return "No";
+
+// Replace the two if statements with one statement, using the && operator, 
+// which will return "Yes" if val is less than or equal to 50 and greater 
+// than or equal to 25. Otherwise, will return "No".
+function testLogicalAnd(val) {
+  // Only change code below this line
+  if (val <= 50 && val >= 25) {
+      return "Yes";
+    }
+  // Only change code above this line
+  return "No";
+}
+// Change this value to test
+testLogicalAnd(10);
+
+
+
+// The logical or operator (||) returns true if either 
+// of the operands is true. Otherwise, it returns false.
+// The logical or operator is composed of two pipe symbols: 
+// (||). This can typically be found between your Backspace and Enter keys.
+// The pattern below should look familiar from prior waypoints:
+
+// if (num > 10) {
+//   return "No";
+// }
+// if (num < 5) {
+//   return "No";
+// }
+// return "Yes";
+
+// will return "Yes" only if num is between 5 and 10 
+// (5 and 10 included). The same logic can be written as:
+
+// if (num > 10 || num < 5) {
+//   return "No";
+// }
+// return "Yes";
+
+// Combine the two if statements into one statement which 
+// returns "Outside" if val is not between 10 and 20, 
+// inclusive. Otherwise, return "Inside".
+function testLogicalOr(val) {
+  // Only change code below this line
+  if (val < 10 || val > 20) {
+    return "Outside";
+  }
+  // Only change code above this line
+  return "Inside";
+}
+// Change this value to test
+testLogicalOr(15);
