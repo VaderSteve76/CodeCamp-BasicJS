ff0333067fcacc7dec7dee9120362066d9eb47e4
VaderSteve76 <alverson.s@yahoo.com>
2020-02-16 16:00:07 -0500

    If, If ELse, Else If statemanets

--- Basic.js
+++ Basic.js
@@ -1331,3 +1331,148 @@ function testLogicalOr(val) {
 }
 // Change this value to test
 testLogicalOr(15);
+
+
+// When a condition for an if statement is true, 
+// the block of code following it is executed. What 
+// about when that condition is false? Normally 
+// nothing would happen. With an else statement, 
+// an alternate block of code can be executed.
+
+// if (num > 10) {
+//   return "Bigger than 10";
+// } else {
+//   return "10 or Less";
+// }
+
+// Combine the if statements into a single if/else statement.
+function testElse(val) {
+  var result = "";
+  // Only change code below this line
+  if (val > 5) {
+    result = "Bigger than 5";
+  } else {
+    result = "5 or Smaller"
+  }
+  // Only change code above this line
+  return result;
+}
+// Change this value to test
+testElse(4);
+
+
+// If you have multiple conditions that need to be addressed, 
+// you can chain if statements together with else if statements.
+
+// if (num > 15) {
+//   return "Bigger than 15";
+// } else if (num < 5) {
+//   return "Smaller than 5";
+// } else {
+//   return "Between 5 and 15";
+// }
+
+// Convert the logic to use else if statements.
+function testElseIf(val) {
+  if (val > 10) {
+    return "Greater than 10";
+  } else if (val < 5) {
+    return "Smaller than 5";
+  } else {
+    return "Between 5 and 10";
+  }
+}
+// Change this value to test
+testElseIf(7);
+
+
+// Order is important in if, else if statements.
+// The function is executed from top to bottom so you 
+// will want to be careful of what statement comes first.
+// Take these two functions as an example.
+
+// Here's the first:
+
+// function foo(x) {
+//   if (x < 1) {
+//     return "Less than one";
+//   } else if (x < 2) {
+//     return "Less than two";
+//   } else {
+//     return "Greater than or equal to two";
+//   }
+// }
+
+// And the second just switches the order of the statements:
+
+// function bar(x) {
+//   if (x < 2) {
+//     return "Less than two";
+//   } else if (x < 1) {
+//     return "Less than one";
+//   } else {
+//     return "Greater than or equal to two";
+//   }
+// }
+
+// While these two functions look nearly identical 
+// if we pass a number to both we get different outputs.
+
+// foo(0) // "Less than one"
+// bar(0) // "Less than two"
+
+// Change the order of logic in the function so that 
+// it will return the correct statements in all cases.
+function orderMyLogic(val) {
+  if(val < 5) {
+    return "Less than 5";
+  } else if (val < 10) {
+    return "Less than 10";
+  } else {
+    return "Greater than or equal to 10";
+  }
+}
+// Change this value to test
+orderMyLogic(7);
+
+
+// if/else statements can be chained together for 
+// complex logic. Here is pseudocode of multiple 
+// chained if / else if statements:
+
+// if (condition1) {
+//   statement1
+// } else if (condition2) {
+//   statement2
+// } else if (condition3) {
+//   statement3
+// . . .
+// } else {
+//   statementN
+// }
+
+// Write chained if/else if statements to 
+// fulfill the following conditions:
+
+// num < 5 - return "Tiny"
+// num < 10 - return "Small"
+// num < 15 - return "Medium"
+// num < 20 - return "Large"
+// num >= 20 - return "Huge"
+function testSize(num) {
+  // Only change code below this line
+  if (num < 5) {
+    return "Tiny";
+  } else if (num < 10) {
+    return "Small";
+  } else if (num < 15) {
+    return "Medium";
+  } else if (num < 20) {
+    return "Large";
+  } else {
+    return "Huge";
+  }
+  // Only change code above this line
+}
+// Change this value to test
+testSize(7);
