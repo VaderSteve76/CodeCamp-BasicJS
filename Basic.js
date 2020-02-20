8fcad83aab5075c228ab47784cdec710c771094d
VaderSteve76 <alverson.s@yahoo.com>
2020-02-20 16:41:37 -0500

    Adding & removing from js objects

--- Basic.js
+++ Basic.js
@@ -1906,3 +1906,117 @@ var testObj = {
 // Only change code below this line
 var entreeValue = testObj["an entree"];   
 var drinkValue = testObj["the drink"];    
+
+
+// Another use of bracket notation on objects is to access a 
+// property which is stored as the value of a variable. This 
+// can be very useful for iterating through an object's properties or when accessing a lookup table.
+// Here is an example of using a variable to access a property:
+
+// var dogs = {
+//   Fido: "Mutt",  Hunter: "Doberman",  Snoopie: "Beagle"
+// };
+// var myDog = "Hunter";
+// var myBreed = dogs[myDog];
+// console.log(myBreed); // "Doberman"
+
+// Another way you can use this concept is when the property's 
+// name is collected dynamically during the program execution, as follows:
+
+// var someObj = {
+//   propName: "John"
+// };
+// function propPrefix(str) {
+//   var s = "prop";
+//   return s + str;
+// }
+// var someProp = propPrefix("Name"); // someProp now holds the value 'propName'
+// console.log(someObj[someProp]); // "John"
+
+// Note that we do not use quotes around the variable name 
+// when using it to access the property because we are 
+// using the value of the variable, not the name.
+// Use the playerNumber variable to look up player 16 in testObj 
+// using bracket notation. Then assign that name to the player variable.
+// Setup
+var testObj = {
+  12: "Namath",
+  16: "Montana",
+  19: "Unitas"
+};
+// Only change code below this line;
+var playerNumber = 16;       
+var player = testObj[playerNumber];   
+
+
+// After you've created a JavaScript object, you can update 
+// its properties at any time just like you would update any 
+// other variable. You can use either dot or bracket notation to update.
+
+// For example, let's look at ourDog:
+
+// var ourDog = {
+//   "name": "Camper",
+//   "legs": 4,
+//   "tails": 1,
+//   "friends": ["everything!"]
+// };
+
+// Since he's a particularly happy dog, let's change his name 
+// to "Happy Camper". Here's how we update his object's name 
+// property: ourDog.name = "Happy Camper"; or ourDog["name"] = "Happy Camper"; 
+// Now when we evaluate ourDog.name, instead of getting "Camper", 
+// we'll get his new name, "Happy Camper".
+// Update the myDog object's name property. Let's change her 
+// name from "Coder" to "Happy Coder". You can use either dot or bracket notation.
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+ourDog.name = "Happy Camper";
+// Setup
+var myDog = {
+  "name": "Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+// Only change code below this line.
+myDog.name = "Happy Coder";
+
+
+// You can add new properties to existing JavaScript 
+// objects the same way you would modify them.
+// Here's how we would add a "bark" property to ourDog:
+
+// ourDog.bark = "bow-wow";
+
+// or
+
+// ourDog["bark"] = "bow-wow";
+
+// Now when we evaluate ourDog.bark, we'll get his bark, "bow-wow".
+// Add a "bark" property to myDog and set it to a dog sound, such as 
+// "woof". You may use either dot or bracket notation.
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+ourDog.bark = "bow-wow";
+// Setup
+var myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+// Only change code below this line.
+myDog.bark = "woof";
