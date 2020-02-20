7fef23745a66c3301e16e211f18ceb0b14ba01dd
VaderSteve76 <alverson.s@yahoo.com>
2020-02-20 16:33:40 -0500

    Dot notation & Bracket notation

--- Basic.js
+++ Basic.js
@@ -1765,3 +1765,144 @@ function abTest(a, b) {
 abTest(2,2);
 
 
+// In the casino game Blackjack, a player can gain an 
+// advantage over the house by keeping track of the relative 
+// number of high and low cards remaining in the deck. 
+// This is called Card Counting. Having more high cards 
+// remaining in the deck favors the player. Each card is 
+// assigned a value according to the table below. When the 
+// count is positive, the player should bet high. When the 
+// count is zero or negative, the player should bet low.
+
+// Count Change	Cards
+// +1	2, 3, 4, 5, 6
+// 0	7, 8, 9
+// -1	10, 'J', 'Q', 'K', 'A'
+
+// You will write a card counting function. It will receive
+// a card parameter, which can be a number or a string, and 
+// increment or decrement the global count variable according 
+// to the card's value (see table). The function will then return 
+// a string with the current count and the string Bet if the count 
+// is positive, or Hold if the count is zero or negative. The current
+// count and the player's decision (Bet or Hold) should be separated by a single space.
+
+// Example Output
+// -3 Hold
+// 5 Bet
+var count = 0;
+
+function cc(card) {
+  // Only change code below this line
+var regex = /[JQKA]/;
+  if (card > 1 && card < 7) {
+    count++;
+  } else if (card === 10 || String(card).match(regex)) {
+    count--;
+  }
+  if (count > 0) return count + " Bet";
+  return count + " Hold";
+}
+// Add/remove calls to test your function.
+// Note: Only the last will display
+cc(2); cc(3); cc(7); cc('K'); cc('A');
+
+
+// You may have heard the term object before.
+// Objects are similar to arrays, except that 
+// instead of using indexes to access and modify 
+// their data, you access the data in objects 
+// through what are called properties.
+// Objects are useful for storing data in a 
+// structured way, and can represent real world objects, like a cat.
+
+// Here's a sample cat object:
+
+// var cat = {
+//   "name": "Whiskers",
+//   "legs": 4,
+//   "tails": 1,
+//   "enemies": ["Water", "Dogs"]
+// };
+
+// In this example, all the properties are stored as 
+// strings, such as - "name", "legs", and "tails". 
+// However, you can also use numbers as properties. 
+// You can even omit the quotes for single-word string properties, as follows:
+
+// var anotherObject = {
+//   make: "Ford",
+//   5: "five",
+//   "model": "focus"
+// };
+
+// However, if your object has any non-string properties, 
+// JavaScript will automatically typecast them as strings.
+// Make an object that represents a dog called myDog which 
+// contains the properties "name" (a string), "legs", "tails" and "friends".
+// You can set these object properties to whatever values you 
+// want, as long "name" is a string, "legs" and "tails" are numbers, and "friends" is an array.
+var myDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+
+// There are two ways to access the properties of an object: 
+// dot notation (.) and bracket notation ([]), similar to an array.
+// Dot notation is what you use when you know the name of 
+// the property you're trying to access ahead of time.
+// Here is a sample of using dot notation (.) to read an object's property:
+
+// var myObj = {
+//   prop1: "val1",
+//   prop2: "val2"
+// };
+// var prop1val = myObj.prop1; // val1
+// var prop2val = myObj.prop2; // val2
+
+// Read in the property values of testObj using dot notation. 
+// Set the variable hatValue equal to the object's property hat 
+// and set the variable shirtValue equal to the object's property shirt.
+// Setup
+var testObj = {
+  "hat": "ballcap",
+  "shirt": "jersey",
+  "shoes": "cleats"
+};
+// Only change code below this line
+var hatValue = testObj.hat;      
+var shirtValue = testObj.shirt;    
+
+
+// The second way to access the properties of an object is 
+// bracket notation ([]). If the property of the object you 
+// are trying to access has a space in its name, you will need to use bracket notation.
+// However, you can still use bracket notation on object properties without spaces.
+// Here is a sample of using bracket notation to read an object's property:
+
+// var myObj = {
+//   "Space Name": "Kirk",
+//   "More Space": "Spock",
+//   "NoSpace": "USS Enterprise"
+// };
+// myObj["Space Name"]; // Kirk
+// myObj['More Space']; // Spock
+// myObj["NoSpace"];    // USS Enterprise
+
+// Note that property names with spaces in them 
+// must be in quotes (single or double).
+// Read the values of the properties "an entree" 
+// and "the drink" of testObj using bracket notation 
+// and assign them to entreeValue and drinkValue respectively.
+// Setup
+var testObj = {
+  "an entree": "hamburger",
+  "my side": "veggies",
+  "the drink": "water"
+};
+// Only change code below this line
+var entreeValue = testObj["an entree"];   
+var drinkValue = testObj["the drink"];    
