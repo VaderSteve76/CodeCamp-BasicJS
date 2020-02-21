d3fd1044d2df965f0f57b93ab41949e75ee47169
VaderSteve76 <alverson.s@yahoo.com>
2020-02-21 17:20:17 -0500

    Changing and looking up objects

--- Basic.js
+++ Basic.js
@@ -2020,3 +2020,75 @@ var myDog = {
 };
 // Only change code below this line.
 myDog.bark = "woof";
+
+
+// We can also delete properties from objects like this:
+// delete ourDog.bark;
+// Delete the "tails" property from myDog. 
+// You may use either dot or bracket notation.
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"],
+  "bark": "bow-wow"
+};
+
+delete ourDog.bark;
+// Setup
+var myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"],
+  "bark": "woof"
+};
+// Only change code below this line.
+delete myDog.tails;
+
+
+// Objects can be thought of as a key/value storage, like a 
+// dictionary. If you have tabular data, you can use an object 
+// to "lookup" values rather than a switch statement or an if/else 
+// chain. This is most useful when you know that your input 
+// data is limited to a certain range.
+// Here is an example of a simple reverse alphabet lookup:
+
+// var alpha = {
+//   1:"Z",
+//   2:"Y",
+//   3:"X",
+//   4:"W",
+//   ...
+//   24:"C",
+//   25:"B",
+//   26:"A"
+// };
+// alpha[2]; // "Y"
+// alpha[24]; // "C"
+
+// var value = 2;
+// alpha[value]; // "Y"
+
+// Convert the switch statement into an object called lookup. 
+// Use it to look up val and assign the associated string to the result variable.
+// Setup
+function phoneticLookup(val) {
+  var result = "";
+  // Only change code below this line
+  var lookup = {
+    "alpha": "Adams",
+    "bravo": "Boston",
+    "charlie": "Chicago",
+    "delta": "Denver",
+    "echo": "Easy",
+    "foxtrot": "Frank"
+  };
+  result = lookup[val];
+  // Only change code above this line
+  return result;
+}
+// Change this value to test
+phoneticLookup("charlie");
+
