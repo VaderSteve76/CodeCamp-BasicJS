30b0ffc43398fecc29a32d2ceb4ba10a41958542
VaderSteve76 <alverson.s@yahoo.com>
2020-02-26 19:47:29 -0500

    Ternary operators

--- Basic.js
+++ Basic.js
@@ -2779,3 +2779,79 @@ function convertToInteger(str) {
   return parseInt(str, 2);
 }
 convertToInteger("10011");
+
+
+// The conditional operator, also called the ternary operator, 
+// can be used as a one line if-else expression.
+
+// The syntax is:
+
+// condition ? statement-if-true : statement-if-false;
+
+// The following function uses an if-else statement to check a condition:
+
+// function findGreater(a, b) {
+//   if(a > b) {
+//     return "a is greater";
+//   }
+//   else {
+//     return "b is greater";
+//   }
+// }
+
+// This can be re-written using the conditional operator:
+
+// function findGreater(a, b) {
+//   return a > b ? "a is greater" : "b is greater";
+// }
+
+// Use the conditional operator in the checkEqual function 
+// to check if two numbers are equal or not. The function 
+// should return either "Equal" or "Not Equal".
+function checkEqual(a, b) {
+  return a === b ? "Equal" : "Not Equal";
+}
+checkEqual(1, 2);
+
+
+// In the previous challenge, you used a single conditional operator. 
+// You can also chain them together to check for multiple conditions.
+
+// The following function uses if, else if, and else statements to check multiple conditions:
+
+// function findGreaterOrEqual(a, b) {
+//   if (a === b) {
+//     return "a and b are equal";
+//   }
+//   else if (a > b) {
+//     return "a is greater";
+//   }
+//   else {
+//     return "b is greater";
+//   }
+// }
+
+// The above function can be re-written using multiple conditional operators:
+
+// function findGreaterOrEqual(a, b) {
+//   return (a === b) ? "a and b are equal" 
+//     : (a > b) ? "a is greater" 
+//     : "b is greater";
+// }
+
+// However, this should be used with care as using multiple conditional 
+// operators without proper indentation may make your code hard to read. 
+// For example:
+
+// function findGreaterOrEqual(a, b) {
+//   return (a === b) ? "a and b are equal" : (a > b) ? "a is greater" : "b is greater";
+// }
+
+// Use multiple conditional operators in the checkSign function to 
+// check if a number is positive, negative or zero. The function 
+// should return "positive", "negative" or "zero".
+function checkSign(num) {
+  return num > 0 ? "positive" : num < 0 ? "negative" : "zero";
+}
+checkSign(10);
+
