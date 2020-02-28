9ea7d3fbb9fe4c71e9ad9780c12a14032a890be0
VaderSteve76 <alverson.s@yahoo.com>
2020-02-28 15:46:13 -0500

    Mutation & prevent mutation

--- Basic.js
+++ Basic.js
@@ -3124,3 +3124,79 @@ function editInPlace() {
   // change code above this line
 }
 editInPlace();
+
+
+// As seen in the previous challenge, const declaration alone 
+// doesn't really protect your data from mutation. To ensure 
+// your data doesn't change, JavaScript provides a function 
+// Object.freeze to prevent data mutation. Once the object is 
+// frozen, you can no longer add, update, or delete properties 
+// from it. Any attempt at changing the object will be rejected without an error.
+
+// let obj = {
+//   name:"FreeCodeCamp",
+//   review:"Awesome"
+// };
+// Object.freeze(obj);
+// obj.review = "bad"; // will be ignored. Mutation not allowed
+// obj.newProp = "Test"; // will be ignored. Mutation not allowed
+// console.log(obj); 
+// // { name: "FreeCodeCamp", review:"Awesome"}
+
+// In this challenge you are going to use Object.freeze to prevent 
+// mathematical constants from changing. You need to freeze the 
+// MATH_CONSTANTS object so that no one is able to alter the 
+// value of PI, add, or delete properties.
+function freezeObj() {
+  'use strict';
+  const MATH_CONSTANTS = {
+    PI: 3.14
+  };
+  // change code below this line
+  Object.freeze(MATH_CONSTANTS);
+  // change code above this line
+  try {
+    MATH_CONSTANTS.PI = 99;
+  } catch(ex) {
+    console.log(ex);
+  }
+  return MATH_CONSTANTS.PI;
+}
+const PI = freezeObj();
+
+
+// In JavaScript, we often don't need to name our functions, 
+// especially when passing a function as an argument to another 
+// function. Instead, we create inline functions. We don't need 
+// to name these functions because we do not reuse them anywhere else.
+// To achieve this, we often use the following syntax:
+
+// const myFunc = function() {
+//   const myVar = "value";
+//   return myVar;
+// }
+
+// ES6 provides us with the syntactic sugar to not have to write 
+// anonymous functions this way. Instead, you can use arrow function syntax:
+
+// const myFunc = () => {
+//   const myVar = "value";
+//   return myVar;
+// }
+
+// When there is no function body, and only a return value, arrow 
+// function syntax allows you to omit the keyword return as well 
+// as the brackets surrounding the code. This helps simplify 
+// smaller functions into one-line statements:
+
+// const myFunc = () => "value";
+
+// This code will still return value by default.
+
+// Rewrite the function assigned to the variable magic which 
+// returns a new Date() to use arrow function syntax. Also 
+// make sure nothing is defined using the keyword var.
+const magic = () => {
+  "use strict";
+  return new Date();
+};
