dc1f1747f0da6b21e67f4a0bac24de48d989985b
VaderSteve76 <alverson.s@yahoo.com>
2020-03-07 16:31:20 -0500

    Importing, exporting and js promises

--- ES6.js
+++ ES6.js
@@ -861,3 +861,67 @@ import * as stringFunctions from "./string_functions.js";
 // add code above this line
 stringFunctions.uppercaseString("hello");
 stringFunctions.lowercaseString("WORLD!");
+
+
+// In the export lesson, you learned about the syntax referred to as a 
+// named export. This allowed you to make multiple functions and variables 
+// available for use in other files. There is another export syntax you need 
+// to know, known as export default. Usually you will use this syntax if only 
+// one value is being exported from a file. It is also used to 
+// create a fallback value for a file or module.
+
+// Below are examples using export default:
+
+// // named function
+// export default function add(x, y) {
+//   return x + y;
+// }
+
+// // anonymous function
+// export default function(x, y) {
+//   return x + y;
+// }
+
+// Since export default is used to declare a fallback value for a module or file, 
+// you can only have one value be a default export in each module or file. Additionally, 
+// you cannot use export default with var, let, or const
+// The following function should be the fallback value for the module. 
+// Please add the necessary code to do so.
+"use strict";
+export default function subtract(x, y) {
+  return x - y;
+}
+
+
+// In the last challenge, you learned about export default and its uses. 
+// To import a default export, you need to use a different import syntax. 
+// In the following example, add is the default export of the 
+// math_functions.js file. Here is how to import it:
+// import add from "./math_functions.js"; The syntax differs in one key 
+// place. The imported value, add, is not surrounded by curly braces ({}). 
+// add here is simply a variable name for whatever the default export of 
+// the math_functions.js file is. You can use any name here when importing a default.
+// In the following code, import the default export from the math_functions.js file, 
+// found in the same directory as this file. Give the import the name subtract.
+import subtract from "./math_functions.js";
+// add code above this line
+subtract(7,4);
+
+
+// A promise in JavaScript is exactly what it sounds like - you use it 
+// to make a promise to do something, usually asynchronously. When the 
+// task completes, you either fulfill your promise or fail to do so. 
+// Promise is a constructor function, so you need to use the new keyword 
+// to create one. It takes a function, as its argument, with two 
+// parameters - resolve and reject. These are methods used to determine 
+// the outcome of the promise. The syntax looks like this:
+
+// const myPromise = new Promise((resolve, reject) => {
+
+// });
+
+// Create a new promise called makeServerRequest. Pass in a function with 
+// resolve and reject parameters to the constructor.
+const makeServerRequest = new Promise((resolve, reject) => {
+
+});
