24b11bc8a8932d71c69865d8604e1e2613788d30
VaderSteve76 <alverson.s@yahoo.com>
2020-03-13 15:49:33 -0400

    Match everything but numbers&letters, matching all numbers and all letters

--- RegExpressions.js
+++ RegExpressions.js
@@ -377,3 +377,48 @@ let result = lastRegex.test(caboose);
 let quoteSample = "The five boxing wizards jump quickly.";
 let alphabetRegexV2 = /\w/g;
 let result = quoteSample.match(alphabetRegexV2).length;
+
+
+// You've learned that you can use a shortcut to match alphanumerics 
+// [A-Za-z0-9_] using \w. A natural pattern you might want to search 
+// for is the opposite of alphanumerics. You can search for the opposite 
+// of the \w with \W. Note, the opposite pattern uses a capital letter. 
+// This shortcut is the same as [^A-Za-z0-9_].
+
+// let shortHand = /\W/;
+// let numbers = "42%";
+// let sentence = "Coding!";
+// numbers.match(shortHand); // Returns ["%"]
+// sentence.match(shortHand); // Returns ["!"]
+
+// Use the shorthand character class \W to count the number of 
+// non-alphanumeric characters in various quotes and strings.
+let quoteSample = "The five boxing wizards jump quickly.";
+let nonAlphabetRegex = /\W/g; // Change this line
+let result = quoteSample.match(nonAlphabetRegex).length;
+
+
+// You've learned shortcuts for common string patterns like alphanumerics. 
+// Another common pattern is looking for just digits or numbers.
+// The shortcut to look for digit characters is \d, with a lowercase d. 
+// This is equal to the character class [0-9], which looks for a single 
+// character of any number between zero and nine.
+// Use the shorthand character class \d to count how many digits are in 
+// movie titles. Written out numbers ("six" instead of 6) do not count.
+let movieName = "2001: A Space Odyssey";
+let numRegex = /\d/g;
+let result = movieName.match(numRegex).length;
+
+
+// The last challenge showed how to search for digits using the shortcut 
+// \d with a lowercase d. You can also search for non-digits using a 
+// similar shortcut that uses an uppercase D instead. The shortcut to 
+// look for non-digit characters is \D. This is equal to the character 
+// class [^0-9], which looks for a single character that is not a number 
+// between zero and nine. Use the shorthand character class for non-digits 
+// \D to count how many non-digits are in movie titles.
+let movieName = "2001: A Space Odyssey";
+let noNumRegex = /\D/g;
+let result = movieName.match(noNumRegex).length;
+
+
