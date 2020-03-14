f2cc74e08a2870c1d51c059d8e65db952c1ceb70
VaderSteve76 <alverson.s@yahoo.com>
2020-03-14 14:32:28 -0400

    Specifying numbers, lower & upper, also exact matches

--- RegExpressions.js
+++ RegExpressions.js
@@ -479,3 +479,72 @@ let result = sample.match(countWhiteSpace);
 let sample = "Whitespace is important in separating words";
 let countNonWhiteSpace = /\S/g; // Change this line
 let result = sample.match(countNonWhiteSpace);
+
+
+// Recall that you use the plus sign + to look for one or more 
+// characters and the asterisk * to look for zero or more characters. 
+// These are convenient but sometimes you want to match a certain 
+// range of patterns. You can specify the lower and upper number of 
+// patterns with quantity specifiers. Quantity specifiers are used with 
+// curly brackets ({ and }). You put two numbers between the curly 
+// brackets - for the lower and upper number of patterns.
+// For example, to match only the letter a appearing between 3 and 5 
+// times in the string "ah", your regex would be /a{3,5}h/.
+
+// let A4 = "aaaah";
+// let A2 = "aah";
+// let multipleA = /a{3,5}h/;
+// multipleA.test(A4); // Returns true
+// multipleA.test(A2); // Returns false
+
+// Change the regex ohRegex to match the entire phrase 
+// "Oh no" only when it has 3 to 6 letter h's.
+let ohStr = "Ohhh no";
+let ohRegex = /Oh{3,6}\sno/;
+let result = ohRegex.test(ohStr);
+
+
+// You can specify the lower and upper number of patterns with 
+// quantity specifiers using curly brackets. Sometimes you only 
+// want to specify the lower number of patterns with no upper limit.
+// To only specify the lower number of patterns, keep the first 
+// number followed by a comma. For example, to match only the 
+// string "hah" with the letter a appearing at least 3 times, 
+// your regex would be /ha{3,}h/.
+
+// let A4 = "haaaah";
+// let A2 = "haah";
+// let A100 = "h" + "a".repeat(100) + "h";
+// let multipleA = /ha{3,}h/;
+// multipleA.test(A4); // Returns true
+// multipleA.test(A2); // Returns false
+// multipleA.test(A100); // Returns true
+
+// Change the regex haRegex to match the word "Hazzah" 
+// only when it has four or more letter z's.
+let haStr = "Hazzzzah";
+let haRegex = /Haz{4,}ah/;
+let result = haRegex.test(haStr);
+
+
+// You can specify the lower and upper number of patterns 
+// with quantity specifiers using curly brackets. Sometimes 
+// you only want a specific number of matches. To specify a 
+// certain number of patterns, just have that one number 
+// between the curly brackets. For example, to match only 
+// the word "hah" with the letter a 3 times, your regex 
+// would be /ha{3}h/.
+
+// let A4 = "haaaah";
+// let A3 = "haaah";
+// let A100 = "h" + "a".repeat(100) + "h";
+// let multipleHA = /ha{3}h/;
+// multipleHA.test(A4); // Returns false
+// multipleHA.test(A3); // Returns true
+// multipleHA.test(A100); // Returns false
+
+// Change the regex timRegex to match the word 
+// "Timber" only when it has four letter m's.
+let timStr = "Timmmmber";
+let timRegex = /Tim{4}ber/;
+let result = timRegex.test(timStr);
